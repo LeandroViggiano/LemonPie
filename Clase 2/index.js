0d6cb0fd9604a9d0a19f52f308bd87dd28469c09
Leandro Viggiano <leandrom.v@outlook.com>
2021-06-08 12:24:36 -0300

    commit1

---
+++ Clase 2/index.js
@@ -0,0 +1,8 @@
+
+var variable1= "hola"; /*puede cambiar de string a int, por ejemplo */
+let variable2; /*Puede cambiar su valor pero no su tipo de dato, siempre int o string */
+const variable3=1; /*Nunca cambia nada*/
+function name(params) {
+    
+}
+console.log("Hola Mundo!");
